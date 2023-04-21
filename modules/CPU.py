507e7642b48145d53d0eff0c99584201fcb4597d
lucein <lucas.dinnouti@sinch.com>
2023-04-20 22:53:08 -0300

    Feature: Add decoding functions

--- modules/CPU.py
+++ modules/CPU.py
@@ -14,5 +14,40 @@ class CPU:
         return 0
     
     def store_program(self, program):
-        for i, inst in enumerate(program):
-            self.bus.store(i, 32, inst)
+        for i, instr in enumerate(program):
+            self.bus.store(i, 32, instr)
+
+#### Decoding instructions
+
+    def rd(self, instr) -> int:
+        return (instr >> 7) & 0x1f
+        
+    def rs1(self, instr) -> int:
+        return (instr >> 15) & 0x1f 
+
+    def rs2(self, instr) -> int:
+        return (instr >> 20) & 0x1f 
+
+    def imm_I(self, instr) -> int:
+        return (instr & 0xfff00000) >> 20 
+
+    def imm_S(self, instr) -> int:
+        return ((instr & 0xfe000000) >> 20) | (instr >> 7) & 0x1f
+
+    def imm_B(self, instr) -> int:
+        return ((instr & 0x80000000) >> 19) \
+            | ((instr & 0x80) << 4) \
+            | ((instr >> 20) & 0x7e0) \
+            | ((instr >> 7) & 0x1e)
+
+    def imm_B(self, instr) -> int:
+        return instr & 0xfffff999
+
+    def imm_J(self, instr) -> int:
+        return ((instr & 0x80000000) >> 11) \
+            | (instr & 0xff000) \
+            | ((instr >> 9) & 0x800) \
+            | ((instr >> 20) & 0x7fe)
+
+    def shamt(self, instr) -> int:
+        return (self.imm_I(instr) & 0x1f) 
